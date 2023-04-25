6716c0a2bf1ed12b4191fa2c85e68900d4b8289d
Fahmedo <faruqahmedolaitan@gmail.com>
2023-04-25 02:31:29 +0100

    in the comoponent folder, the div in line 9 to 15 was not well formatted,but i have corrected that

--- app/(Components)/Subscribe.js
+++ app/(Components)/Subscribe.js
@@ -1,17 +1,20 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 function Subscribe() {
   return (
     <div className="subscribeContainer">
-        <h1>Subscribe</h1>
-        <h2>Subscribe to our podcast and get the latest episodes delivered to your inbox.</h2>
-        <div className="subscribeForm">
-            <input type="text" placeholder="Your Email Address" />
-            <button>Subscribe</button>
-            </div>
+      <h1>Subscribe</h1>
+      <h2>
+        Subscribe to our podcast and get the latest episodes delivered to your
+        inbox.
+      </h2>
+      <div className="subscribeForm">
+        <input type="text" placeholder="Your Email Address" />
+        <button>Subscribe</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Subscribe
+export default Subscribe;
